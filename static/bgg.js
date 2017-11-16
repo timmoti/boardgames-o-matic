311b6bb78acda2dc91642f04ffa43b691b9f25ef
TimothyTan <timmoti@gmail.com>
2017-11-17 01:36:11 +0800

    upload part 3

--- static/bgg.js
+++
@@ -1,7 +0,0 @@
-function submit_toggle() {
-    var x = document.getElementById("explore");
-    var y = document.getElementById("form");
-    
-    y.style.display = "none";
-    x.style.display = "block";
-}
